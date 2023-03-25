fbea637b694d097ac1b719159f886de1814d9f0c
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2023-03-25 10:55:09 +0600

    add: утилиты для создания файлов и урлов.

---
+++ programs/generate-filename.py
@@ -0,0 +1,12 @@
+import re
+
+def transform_string(s):
+    s = s.lower()  # Преобразуем строку к lower case
+    s = s.replace(" ", "-")  # Заменяем пробелы на "-"
+    s = s.replace("_", "-")  # Заменяем знаки "_" на "-"
+    s = re.sub(r'[^\w-]', '', s)  # Удаляем все знаки препинания, кроме "-", "_" и буквенно-цифровых символов
+    s = re.sub(r'(-{2,})', '-', s)  # Убираем повторяющиеся символы "-"
+    return s
+
+s = transform_string('Take a Ten Minutes Walk')
+print(s)
---
+++ programs/generate-url.py
@@ -0,0 +1,18 @@
+import re
+
+def transform_string(input_string):
+    # Удаляем все знаки препинания
+    cleaned_string = re.sub(r'[^\w\s]','',input_string)
+    # Приводим все символы к нижнему регистру
+    lowercase_string = cleaned_string.lower()
+    # Словарь для замены кириллических символов на латинские
+    cyrillic_to_latin = {'а':'a', 'б':'b', 'в':'v', 'г':'g', 'д':'d', 'е':'e', 'ё':'yo', 'ж':'zh', 'з':'z', 'и':'i', 'й':'j', 'к':'k', 'л':'l', 'м':'m', 'н':'n', 'о':'o', 'п':'p', 'р':'r', 'с':'s', 'т':'t', 'у':'u', 'ф':'f', 'х':'h', 'ц':'c', 'ч':'ch', 'ш':'sh', 'щ':'shch', 'ъ':'', 'ы':'y', 'ь':'', 'э':'e', 'ю':'yu', 'я':'ya'}
+    # Заменяем кириллические символы на латинские
+    transliterated_string = ''.join([cyrillic_to_latin.get(c, c) for c in lowercase_string])
+    # Заменяем пробелы на "-"
+    result_string = re.sub(r'\s+', '-', transliterated_string)
+    return result_string
+
+text = transform_string('нелинейные-уравнения')
+
+print(text)
